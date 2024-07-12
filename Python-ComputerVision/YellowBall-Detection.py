5a2432f4965aea199e1ee7099c1efa0fbf0a3900
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-07-12 10:15:01 -0400

    Updated color comments for tutorial

--- Python-ComputerVision/YellowBall-Detection.py
+++ Python-ComputerVision/YellowBall-Detection.py
@@ -8,7 +8,7 @@ from time import sleep
 def detect_yellow_ball():
     # Start capturing video from the webcam. If multiple webcams connected, you may use 1,2, etc.
     cap = cv.VideoCapture(0)
-    # CAP_PROP_FPS sets the frame rate of the webcam to 30 fps here
+    # *1 CAP_PROP_FPS sets the frame rate of the webcam to 30 fps here
     cap.set(cv.CAP_PROP_FPS, 30)
     
     while True:
@@ -18,20 +18,20 @@ def detect_yellow_ball():
             print("Failed to grab frame")
             break
         
-        # Set the image resolution to 480x480 *** Note increasing resolution increases processing power used, and may slow down video feed
+        # *2 Set the image resolution to 480x480. Note increasing resolution increases processing power used, and may slow down video feed.
         frame = cv.resize(frame, (480, 480))
 
         # Convert the frame from BGR to HSV color space to easily identify a colour
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) 
 
-        # Define the range of yellow color in HSV [Hue, Saturation, Value] ***
-        # You can get these values via the method explained in the tutorial. You can also change the variable names to suit your color ball
-        lower_yellow = np.array([20, 100, 100])
-        upper_yellow = np.array([30, 255, 255])
+        # *3 Define the range of yellow color in HSV [Hue, Saturation, Value]
+        # SET THESE VALUES VIA THE METHOD EXPLAINED IN THE TUTORIAL
+        ball_color_lower = np.array([20, 100, 100]) # [lower Hue, lower Saturation, lower Value]
+        ball_color_upper = np.array([30, 255, 255]) # [upper Hue, upper Saturation, upper Value]
 
-        # Threshold the HSV image to get only yellow colors
+        # Threshold the HSV image to get the colors defined above
         # Pixels in the range are set to white (255) and those that aren't are set to black (0), creating a binary mask 
-        mask = cv.inRange(hsv, lower_yellow, upper_yellow)
+        mask = cv.inRange(hsv, ball_color_lower, ball_color_upper)
 
         # Find contours in the mask
         # RETR_TREE retrieves all hierarchical contours and organizes them
@@ -44,10 +44,11 @@ def detect_yellow_ball():
             largest_contour = max(contours, key=cv.contourArea)
             # Computes the minimum enclosing circle aroudn the largest contour
             ((x, y), radius) = cv.minEnclosingCircle(largest_contour)
-            if radius > 10:  # Only consider large enough objects *** If it only detects a small portion of your ball, you can test higher radius values to capture more of the ball
-                # Draw a circle around the yellow ball
+            # * 4 Only consider large enough objects. If it only detects a small portion of your ball, you can test higher radius values to capture more of the ball
+            if radius > 10:
+                # Draw a yellow circle around the ball
                 cv.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                # Draw a dot in the center of the yellow ball
+                # Draw a red dot in the center of the ball
                 cv.circle(frame, (int(x), int(y)), 2, (0, 0, 255), -1)  # (image to draw dot on, x,y pixel coordinates, radius in pixels, RGB values in this case red, -1 indicates to fill the circle)
                 # Display the position of the ball
                 print(f"Yellow ball detected at position: ({int(x)}, {int(y)})")
