80ce72aa65b8e8c471e11f7acd7c93659552187f
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-07-23 15:16:42 -0400

    Changed delays & added serial debugging on Pi & Mega

--- IntegrationTesting/i2cBlockDataTest3.py
+++ IntegrationTesting/i2cBlockDataTest3.py
@@ -5,7 +5,7 @@ import struct
 addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/i2c-1
 
-steps = [600, 600, 600]
+steps = [1000, 1000, 1000]
 speeds = [500, 500, 500]
 accels = [200, 200, 200]
 ByteSteps = [0,0,0]
@@ -17,28 +17,28 @@ for i in range(0,3):
     ByteSpeeds[i] = list(struct.pack('>h', speeds[i]))
     ByteAccels[i] = list(struct.pack('>h', accels[i]))
     
-# for i in range (0,3):
-#     for j in range (0,2):
-#         print(f"ByteSteps[",i,"][",j,"] = ", ByteSteps[i][j])
-#         sleep(0.001)
-#     for j in range (0,2):
-#         print(f"ByteSpeeds[",i,"][",j,"] = ", ByteSpeeds[i][j])
-#         sleep(0.001)
-#     for j in range (0,2):
-#         print(f"ByteAccels[",i,"][",j,"] = ", ByteAccels[i][j])
-#         sleep(0.001)
+for i in range (0,3):
+    for j in range (0,2):
+        print(f"ByteSteps[",i,"][",j,"] = ", ByteSteps[i][j])
+        sleep(0.001)
+    for j in range (0,2):
+        print(f"ByteSpeeds[",i,"][",j,"] = ", ByteSpeeds[i][j])
+        sleep(0.001)
+    for j in range (0,2):
+        print(f"ByteAccels[",i,"][",j,"] = ", ByteAccels[i][j])
+        sleep(0.001)
 
 while True:
     for i in range (0,3):
         for j in range (0,2):
             bus.write_byte(addr, ByteSteps[i][j])
-            sleep(0.001)
+            sleep(0.5)
         for j in range (0,2):
             bus.write_byte(addr, ByteSpeeds[i][j])
-            sleep(0.001)
+            sleep(0.5)
         for j in range (0,2):
             bus.write_byte(addr, ByteAccels[i][j])
-            sleep(0.001)
+            sleep(0.5)
     sleep(3)
 
 # while True:
