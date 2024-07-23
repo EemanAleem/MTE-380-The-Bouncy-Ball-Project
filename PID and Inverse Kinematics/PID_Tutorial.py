5b4cbfc8df5170ffbfccd03e1a0f46b8d52b3bac
jakej0 <145575318+jakej0@users.noreply.github.com>
2024-07-23 16:30:09 -0400

    Update PID_Tutorial.py

--- PID and Inverse Kinematics/PID_Tutorial.py
+++ PID and Inverse Kinematics/PID_Tutorial.py
@@ -7,9 +7,10 @@ const int enPin = 4;
 int directionMultiplier = 1;
 // PID constants
 float kp = 1;
-float kd = 0.025;
-float ki = 0.0;
-
+float kd = 0;
+float ki = 0;
+int output = 0;
+int outputPrev = 0;
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
 
 long prevT = 0;
@@ -18,8 +19,9 @@ float integral = 0;
 
 void setup() {
   Serial.begin(9600);
-  stepper.setMaxSpeed(6400); // Speed = Steps / second
-  stepper.setAcceleration(800); // Acceleration = Steps /(second)^2
+  stepper.setMaxSpeed(128000); // Speed = Steps / second
+  stepper.setAcceleration(12800); // Acceleration = Steps /(second)^2
+  stepper.setSpeed(128000);
   stepper.disableOutputs(); // Disable outputs initially
   stepper.setCurrentPosition(0);
   stepper.enableOutputs();
@@ -32,7 +34,8 @@ void loop() {
 void PID() {
 
   int target = 360*sin(prevT/4e6) * 3200 / 360;
-
+  Serial.print(target);
+  Serial.print(",");
   // time difference
   long currT = micros();
   float deltaT = ((float) (currT - prevT))/( 1.0e6 );
@@ -40,16 +43,26 @@ void PID() {
 
   // PID calculation
   int error = stepper.currentPosition() - target;
-  integral += integral + error*deltaT;
+  integral = integral + error*deltaT;
   float derivative = (error - errorPrev)/(deltaT);
+  errorPrev = error;
 
   // control signal
-  float output = kp*error + kd*derivative + ki*integral;
+  output = -(kp*error + kd*derivative + ki*integral);
+  Serial.print(output);
+  Serial.print(",");
 
-  if (output > 0)
+  if (output < outputPrev)
     directionMultiplier = -1;
   else 
     directionMultiplier = 1;
-  stepper.moveTo(directionMultiplier * output);
-  stepper.run();
+
+  outputPrev = output;
+  stepper.moveTo(output);
+  stepper.setSpeed(directionMultiplier * 51200);
+  while(stepper.currentPosition() != output) 
+    stepper.runSpeed();
+  
+  Serial.print(stepper.currentPosition());
+  Serial.println();
 }
