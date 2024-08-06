1a06624355ecd963c62b9050cae537f2db109fa4
jakej0 <145575318+jakej0@users.noreply.github.com>
2024-08-06 10:49:41 -0700

    Create LEDTest_PI.py

---
+++ Tutorial Files/LEDTest_PI.py
@@ -0,0 +1,22 @@
+from smbus2 import SMBus
+
+# Set bus address to 0x8
+addr = 0x8 # bus address
+bus = SMBus(1) # indicates /dev/i2c-1. Use this bus.
+
+number = 1 # bool flag to stop while loop below
+
+print("Enter 1 for ON or 0 for OFF")
+
+# if user inputs an integer other than 1 or 0, program ends
+while number == 1:
+    ledstate = input(">>>>>>>     ")
+    # Switch on
+    if ledstate == "1":
+	  # Sends a byte of data 0x1 to address ‘addr’
+        bus.write_byte(addr, 0x1)
+    # Switch off
+    elif ledstate == "0":
+        bus.write_byte(addr, 0x0)
+    else:
+        number = 0
