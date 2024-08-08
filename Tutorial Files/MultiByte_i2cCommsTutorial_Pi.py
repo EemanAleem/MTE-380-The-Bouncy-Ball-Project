b09962fd253cc8fc751b2d7b8134a64531c1f448
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-08-08 11:38:56 -0400

    Update MultiByte_i2cCommsTutorial_Pi.py

--- Tutorial Files/MultiByte_i2cCommsTutorial_Pi.py
+++ Tutorial Files/MultiByte_i2cCommsTutorial_Pi.py
@@ -6,19 +6,17 @@ addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/i2c-1
 sleep(2)
 
-LargeNum = [500, 1000, 1500]
-ByteLargeNum = [0,0,0]
+steps = [500, 1000, 1500]
+ByteSteps = [0,0,0]
 
 sleep(3)
 
 while True:
     for i in range (0,3):
-	# Converts each number into a 2-byte format
-        ByteLargeNum[i] = list(struct.pack('>h', LargeNum[i]))
+        ByteSteps[i] = list(struct.pack('>h', steps[i]))
         for j in range (0,2):
-	    # Prints each byte of the 2 byte format and sends it to the I2C device
-            print("ByteLargeNum[", i, "][", j, "]: ", ByteLargeNum[i][j])
-            bus.write_byte(addr, ByteLargeNum[i][j])
+            print("ByteSteps[", i, "][", j, "]: ", ByteSteps[i][j])
+            bus.write_byte(addr, ByteSteps[i][j])
             sleep(0.05)
-        print("LargeNum[", i, "]: ", LargeNum[i])
-        LargeNum[i] = LargeNum[i] + 2
+        print("steps[", i, "]: ", steps[i])
+        steps[i] = steps[i] + 2
