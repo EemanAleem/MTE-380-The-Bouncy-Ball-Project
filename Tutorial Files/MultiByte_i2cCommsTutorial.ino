30c84a2d9d9294487ada30689c775afe3020d50a
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-08-09 11:56:25 -0400

    Added more detailed comments

--- Tutorial Files/MultiByte_i2cCommsTutorial.ino
+++ Tutorial Files/MultiByte_i2cCommsTutorial.ino
@@ -5,18 +5,18 @@ const int SDA_Pin = 20;
 const int SCL_Pin = 21;
 
 // Data parsing in receiveEvent: ******************************************************************
-// receivedByte is byte received from transmission; byte1 and byte2 are temporary values for receivedValue;
+// receivedByte is the byte received from transmission; byte1 and byte2 are temporary values for receivedValue;
 // countByte is used to check whether we have received a first or second byte;
-// receivedValue is two bytes combined with a bitwise operation to get an integer in the range -32,768 to 32,767
+// receivedValue is two bytes combined with a bitwise operation to get an integer in the range 0 to 65,534.
 volatile long int receivedByte, receivedValue;
-volatile long int byte1, byte2;
-volatile long long int countByte = 1, countValue = 1;
+volatile int byte1, byte2;
 volatile long int steps[3];
+volatile unsigned long long int countByte = 1, countValue = 1;
  
 void setup() {
   Wire.begin(0x8); // Arduino joins I2C bus as slave with address 8
-  Wire.setClock(400000);  // Set clock speed to 400 kHz
-  Serial.begin(9600);
+  Wire.setClock(400000);  // Set clock speed to 400 kHz, increases speed of transmissions
+  Serial.begin(9600); //Sets up serial monitor to display received bytes and values
   
   // Call receiveEvent function when data received                
   Wire.onReceive(receiveEvent);
@@ -30,19 +30,28 @@ void setup() {
 void receiveEvent(int howMany) {
   receivedByte = Wire.read();
 
-  // If the counter is odd, therefore a first byte.
+  // If the byte counter is odd, it's the first byte
   if (countByte%2 != 0)
     byte1 = receivedByte;
+  // If the byte counter is even, it's the second byte
   else {
     byte2 = receivedByte;
+
+    // Display received bytes
     Serial.print("byte 1: ");
     Serial.println(byte1);
     Serial.print("byte 2: ");
     Serial.println(byte2);
 
-    // byte1 << 8 shifts the bits of byte1 to the left by 8 positions. This effectively multiplies byte1 by 256 and places it in the high byte of a 16-bit integer.
-    // The bitwise OR operation combines the shifted byte1 with byte2 to form the final 16-bit value. byte2 is placed in the lower 8 bits of the result.
+    // byte1 << 8 shifts the bits of byte1 to the left by 8 positions, making it the MSB. 
+    // This effectively multiplies byte1 by 256 and places it in the high byte of a 16-bit integer.
+    // The bitwise OR operation combines the MSB byte1 with the now LSB byte2 to form the final 16-bit value.
+    // General format: variable = (MSB << 8) | LSB;
     receivedValue = (byte1 << 8) | byte2;
+
+    // If the value counter follows the sequence in the condition below, it's the step[0] value so save it there.
+    // The other conditions follow the same logic as the first. 
+    // This is needed to accurately determine what transmission is slotted for what array value.
     if ( (countValue - 1) % 3 == 0 ) {
       steps[0] = receivedValue;
       Serial.print("steps[0]: ");
@@ -58,12 +67,15 @@ void receiveEvent(int howMany) {
       Serial.print("steps[2]: ");
       Serial.println(steps[2]);
     }
+    // Increment the Value counter.
     countValue++;
   }
+  // Increment the Byte counter
   countByte++;
+  // Add a delay between transmissions. *1
   delay(20);
 }
 
 void loop() {
-  delay(100); // Keep waiting for data
+  delay(100); // Or add a delay between transmissions here *2
 }
