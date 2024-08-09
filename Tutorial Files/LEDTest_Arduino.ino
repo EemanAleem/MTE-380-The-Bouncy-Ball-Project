2439add1f454ff776694c48e18d1af380cbafd7f
Eeman Aleem <113636413+EemanAleem@users.noreply.github.com>
2024-08-09 13:22:31 -0400

    Removed while loop in receiveEvent

--- Tutorial Files/LEDTest_Arduino.ino
+++ Tutorial Files/LEDTest_Arduino.ino
@@ -1,10 +1,13 @@
 #include <Wire.h>
 
-const int ledPin = 13; 
+const int SDA_Pin = 20;
+const int SCL_Pin = 21;
+const int ledPin = 47; 
  
 void setup() {
   // Arduino joins I2C bus as slave with address 8
   Wire.begin(0x8);
+  Serial.begin(9600);
   
   // Call receiveEvent function when data received                
   Wire.onReceive(receiveEvent);
@@ -12,19 +15,16 @@ void setup() {
   // Setup pin 13 as output and turn LED off at the beginning
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-
-  // Turn off 20k-50k ohm built-in pull up resistors at pins specified
+  
   digitalWrite(SDA_Pin, LOW);
   digitalWrite(SCL_Pin, LOW);
-
 }
  
 // Function that executes whenever data is received from master device, the Pi 5
 void receiveEvent(int howMany) {
-  while (Wire.available()) { // Loop until I2C connection unavailable
-    int c = Wire.read(); // receive byte as an integer
-    digitalWrite(ledPin, c); // turn on/off LED based on byte information
-  }
+  int c = Wire.read(); // receive byte as an integer
+  digitalWrite(ledPin, c); // turn on/off LED based on byte information
+  Serial.println(c);
 }
 
 void loop() {
